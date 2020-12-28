f9a54c69fb1b58af580bc644d863978d7ad90a2f
Anaesthasia Patricia <56495997+anpatricia06@users.noreply.github.com>
2020-12-28 16:03:03 +0700

    I created tch_add_form.py

---
+++ final_exam/school/controllers/tch_add_form.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from school.models.student import Teacher
+#ini buat bagian form
+
+def add_teacher(request):
+    if request.method == 'POST':
+        form = TeacherForm(request.POST)
+        if form.is_valid():
+            form.save()  # directly save the form
+            return HttpResponseRedirect(reverse('teachers'))
+    else:
+        form = TeacherForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'teachers_form.html', context=context)
