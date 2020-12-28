a52a7f7d2bb21de4af146ee8f2fa1efc9abe64ac
vikaahtrk <76093356+vikaahtrk@users.noreply.github.com>
2020-12-28 15:17:14 +0700

    i updated index_controller.py

--- final_exam/school/controllers/index_controller.py
+++ final_exam/school/controllers/index_controller.py
@@ -1,17 +1,14 @@
-#buat bagian home dan ada tampilan bagian session
 from django.shortcuts import render
 from school.models.student import Student
 from school.models.teacher import Teacher
 
 def index(request):
-   #print(request.session) #on the request
-   #print(dir(request.session))
-    
+  
     num_visit= request.session.get('num_visit', 1)
     request.session['num_visit']= num_visit + 1
     
-    num_student = Student.object.all().count()
-    num_teacher = Teacher.object.all().count()
+    num_student = Student.objects.all().count()
+    num_teacher = Teacher.objects.all().count()
     context = {
         'num_student': num_student,
         'num_teacher': num_teacher,
