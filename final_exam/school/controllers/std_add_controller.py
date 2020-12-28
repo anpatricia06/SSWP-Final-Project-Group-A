0ab9c26e1fb59be4a77c858c26e44cb4ed025835
Anaesthasia Patricia <56495997+anpatricia06@users.noreply.github.com>
2020-12-28 15:29:35 +0700

    I created std_add_controller.py

---
+++ final_exam/school/controllers/std_add_controller.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from school.models.student import Student
+#bagian sini buat import dari bagian form
+
+def add_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()  # directly save the form
+            return HttpResponseRedirect(reverse('students'))
+    else:
+        form = StudentForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'student_form.html', context=context)
