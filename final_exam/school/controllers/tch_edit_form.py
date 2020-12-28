7422fcf0e7fb4925779fca58ea61584522e20a22
Anaesthasia Patricia <56495997+anpatricia06@users.noreply.github.com>
2020-12-28 16:04:25 +0700

    I created tch_edit_form.py

---
+++ final_exam/school/controllers/tch_edit_form.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from school.models.student import Student
+#ini bagian buat import dari form
+
+def edit_teacher(request, teacher_id):
+    if request.method == 'POST':
+        teacher = Teacher.objects.get(pk=teacher_id)
+        form = TeacherForm(request.POST, instance=teacher)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('teachers'))
+    else:
+        teacher = Teacher.objects.get(pk=teacher_id)
+        fields = model_to_dict(teacher)
+        form = TeacherForm(initial=fields, instance=teacher)
+    context = {
+        'form': form,
+        'type': 'edit',
+    }
+    return render(request, 'teacher_form.html', context=context)
