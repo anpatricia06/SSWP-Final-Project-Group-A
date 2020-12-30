2d1d2399d92a9f542a852cafad4855a843f64cdf
Anaesthasia Patricia <56495997+anpatricia06@users.noreply.github.com>
2020-12-30 14:26:29 +0700

    i updated and renamed tch_edit_form.py to tch_edit_controller.py

--- final_exam/school/controllers/tch_edit_controller.py
+++ final_exam/school/controllers/tch_edit_controller.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from school.models.student import Teacher
-#ini bagian buat import dari form
+from school.models.teacher import Teacher
+from school.forms import TeacherForm
 
 def edit_teacher(request, teacher_id):
     if request.method == 'POST':
