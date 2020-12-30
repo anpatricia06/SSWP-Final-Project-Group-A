803aaf49fb8223986c8a7db541b2a3f3f7de2014
Anaesthasia Patricia <56495997+anpatricia06@users.noreply.github.com>
2020-12-30 14:24:23 +0700

    i updated tch_del_controller.py

--- final_exam/school/controllers/tch_del_controller.py
+++ final_exam/school/controllers/tch_del_controller.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from school.models.student import Teacher
-#ini bagian buat import dari form
+from school.models.teacher import Teacher
+from school.forms import TeacherForm
 
 def delete_teacher(request, teacher_id):
     teacher = Teacher.objects.get(pk=teacher_id)
