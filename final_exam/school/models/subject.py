265add1fb28bfde571adfa52a17019005c2d800e
Anaesthasia Patricia <56495997+anpatricia06@users.noreply.github.com>
2020-12-30 19:18:59 +0700

    i edited subject.py

--- final_exam/school/models/subject.py
+++ final_exam/school/models/subject.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Subject(models.Model):
-    name: models.CharField(max_length=20)
+    name= models.CharField(max_length=20)
 
     class Meta:
         app_label= 'school'
