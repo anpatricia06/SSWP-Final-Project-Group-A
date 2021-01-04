d0cb5af63fcb265cd6e430b5b12b9920ca9d0bbc
Anaesthasia Patricia <56495997+anpatricia06@users.noreply.github.com>
2021-01-04 18:14:59 +0700

    i updated student_controller.py

--- final_exam/school/controllers/student_controller.py
+++ final_exam/school/controllers/student_controller.py
@@ -3,7 +3,12 @@ from school.models.student import Student
 from school.forms import StudentForm
 
 def list_students(request):
-    students= Student.objects.all()
+    if request.method == 'POST':
+        req = request.POST.dict()
+        s_name = req['s_name']
+        students = Student.objects.filter(name__contains=s_name)
+    else:
+        students= Student.objects.all()
     context = {
         'students': students,
     }
