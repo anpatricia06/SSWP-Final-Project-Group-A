9cf2f83cc8abcf9b726038389fdb15d5b39a3a76
justinuscjulian <38638637+justinuscjulian@users.noreply.github.com>
2021-01-05 15:35:18 +0700

    Update forms.py

--- final_exam/school/forms.py
+++ final_exam/school/forms.py
@@ -6,6 +6,8 @@ from school.models.student_id import StudentId
 from school.models.teacher_id import TeacherId
 from school.models.subject import Subject
 from school.models.position import TeacherPosition
+from django.conf import settings
+from school.models.oxford import Dictionary
 
 class StudentForm(ModelForm):
     class Meta:
@@ -16,3 +18,25 @@ class TeacherForm(ModelForm):
     class Meta:
         model= Teacher
         fields=['teacher_id', 't_name', 'subject', 't_dob', 't_address', 't_phonenum', 't_email', 'position']
+
+class DictionaryForm(ModelForm):
+    class Meta:
+        model= Dictionary
+
+        def search(self):
+            result = {}
+            word = self.cleaned_data['word']
+            endpoint = 'https://od-api.oxforddictionaries.com/api/v1/entries/{source_lang}/{word_id}'
+            url = endpoint.format(source_lang='en', word_id=word)
+            headers = {'app_id': settings.OXFORD_APP_ID, 'app_key': settings.OXFORD_APP_KEY}
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:  # SUCCESS
+                result = response.json()
+                result['success'] = True
+            else:
+                result['success'] = False
+            if response.status_code == 404:  # NOT FOUND
+                result['message'] = 'No entry found for "%s"' % word
+            else:
+                result['message'] = 'The Oxford API is not available at the moment. Please try again later.'
+            return result
