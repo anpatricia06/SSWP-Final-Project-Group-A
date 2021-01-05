0bd468a19867dd48f6244110b00a6d52b97863f5
justinuscjulian <38638637+justinuscjulian@users.noreply.github.com>
2021-01-05 15:44:28 +0700

    i created extracurricular.py

---
+++ final_exam/school/models/extracurricular.py
@@ -0,0 +1,10 @@
+from django.db import models
+ 
+class Extracurricular(models.Model):
+       name= models.CharField(max_length=30, default='SOME STRING')
+ 
+    class Meta:
+        app_label= 'school'
+ 
+    def __str__(self):
+        return f'{self.name}'
