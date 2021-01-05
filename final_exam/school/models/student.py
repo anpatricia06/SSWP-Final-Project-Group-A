d4a3a0f665be7e54e79c596fe246d8081b05777f
Anaesthasia Patricia <56495997+anpatricia06@users.noreply.github.com>
2021-01-05 21:41:05 +0700

    i updated student.py

--- final_exam/school/models/student.py
+++ final_exam/school/models/student.py
@@ -1,6 +1,7 @@
 from django.db import models
 from school.models.student_id import StudentId
 from school.models.teacher_id import TeacherId
+from school.models.extracurricular import Extracurricular
 
 class Student(models.Model):
     student_id= models.OneToOneField(StudentId, on_delete=models.CASCADE)
@@ -9,10 +10,12 @@ class Student(models.Model):
     s_address= models.CharField('Address',max_length=50)
     s_phonenum= models.CharField('Phone Number:',max_length=20)
     s_email= models.CharField('E-mail',max_length=30)
+    s_class= models.CharField('Class',max_length=10)
     teacher_id= models.ForeignKey(TeacherId, on_delete=models.CASCADE)
+    extracurricular = models.ManyToManyField(Extracurricular)
     
     class Meta:
         app_label= 'school'
 
     def __str__(self):
-        return f'{self.s_name}'
+        return f'{self.s_name}, {self.s_class}'
