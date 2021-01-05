6e704fcf5337dc1266af5c7817a8bac179b8a658
Anaesthasia Patricia <56495997+anpatricia06@users.noreply.github.com>
2021-01-05 22:23:29 +0700

    Update extracurricular.py

--- final_exam/school/models/extracurricular.py
+++ final_exam/school/models/extracurricular.py
@@ -1,8 +1,8 @@
 from django.db import models
  
 class Extracurricular(models.Model):
-       name= models.CharField(max_length=30, default='SOME STRING')
- 
+    name= models.CharField(max_length=30, default='SOME STRING')
+    
     class Meta:
         app_label= 'school'
  
