502665243a281261cc50a228ee3b05011fd4a727
Anaesthasia Patricia <56495997+anpatricia06@users.noreply.github.com>
2021-01-05 21:27:12 +0700

    i updated forms.py

--- final_exam/school/forms.py
+++ final_exam/school/forms.py
@@ -8,6 +8,7 @@ from school.models.subject import Subject
 from school.models.position import TeacherPosition
 from django.conf import settings
 from school.models.oxford import Dictionary
+import requests
 
 class StudentForm(ModelForm):
     class Meta:
@@ -22,21 +23,22 @@ class TeacherForm(ModelForm):
 class DictionaryForm(ModelForm):
     class Meta:
         model= Dictionary
+        fields=['word']
 
-        def search(self):
-            result = {}
-            word = self.cleaned_data['word']
-            endpoint = 'https://od-api.oxforddictionaries.com/api/v1/entries/{source_lang}/{word_id}'
-            url = endpoint.format(source_lang='en', word_id=word)
-            headers = {'app_id': settings.OXFORD_APP_ID, 'app_key': settings.OXFORD_APP_KEY}
-            response = requests.get(url, headers=headers)
-            if response.status_code == 200:  # SUCCESS
-                result = response.json()
-                result['success'] = True
-            else:
-                result['success'] = False
+    def search(self):
+        result = {}
+        word = self.cleaned_data['word']
+        endpoint = 'https://od-api.oxforddictionaries.com/api/v2/entries/{source_lang}/{word_id}'
+        url = endpoint.format(source_lang='en', word_id=word)
+        headers = {'app_id': settings.OXFORD_APP_ID, 'app_key': settings.OXFORD_APP_KEY}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:  # SUCCESS
+            result = response.json()
+            result['success'] = True
+        else:
+            result['success'] = False
             if response.status_code == 404:  # NOT FOUND
                 result['message'] = 'No entry found for "%s"' % word
             else:
                 result['message'] = 'The Oxford API is not available at the moment. Please try again later.'
-            return result
+        return result
