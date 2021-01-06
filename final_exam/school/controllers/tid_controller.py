98aff6d05da6f7abe24646470054340116bc671f
Anaesthasia Patricia <56495997+anpatricia06@users.noreply.github.com>
2021-01-06 22:21:08 +0700

    Update tid_controller.py

--- final_exam/school/controllers/tid_controller.py
+++ final_exam/school/controllers/tid_controller.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from school.models.teacher_id import TeacherId
 from school.forms import TeacherIdForm
  
