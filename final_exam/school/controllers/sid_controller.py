4f28af78f8ba66fed9b5b76280531270232c0a9b
Anaesthasia Patricia <56495997+anpatricia06@users.noreply.github.com>
2021-01-06 22:15:46 +0700

    Update sid_controller.py

--- final_exam/school/controllers/sid_controller.py
+++ final_exam/school/controllers/sid_controller.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from school.models.student_id import StudentId
 from school.forms import StudentIdForm
  
